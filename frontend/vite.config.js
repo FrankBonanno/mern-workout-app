d9c8c639304f1755c2a1b5ba136ef96856161e59
FrankBonanno <fb0023@mix.wvu.edu>
2022-11-12 17:34:06 -0500

    proxy changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
+        port: 4000,
         proxy: {
-            '/api': 'https://workout-tracker-fb.onrender.com/',
+            '/api': 'https://workout-tracker-fb.onrender.com',
         },
     },
     plugins: [react()],
